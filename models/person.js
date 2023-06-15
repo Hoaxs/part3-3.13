b4d911cd0e2d01b3ba81ac6a5087a7fdbbb3bb09
Stanley Okoye <oakstanley@yahoo.com>
2023-06-15 10:32:21 +0200

    Update person.js

--- models/person.js
+++ models/person.js
@@ -28,4 +28,4 @@ personSchema.set('toJson', {
         delete returnedObject.__v
     }
 })
-module.exports = mongoose.model('person', personSchema, phonebook)
+module.exports = mongoose.model('person', personSchema, 'phonebook')
